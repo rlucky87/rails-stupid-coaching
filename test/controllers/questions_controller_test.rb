b0de92469071928d1da9fb807991cdebcf268cde
Ruben Hernandez Dominguez <r1987hd@gmail.com>
2022-11-14 13:54:02 +0100

    stupidcoaching

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
-class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+# class QuestionsControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
+
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
 end
